fb8b9c8ea9b656c7cd9793a90a676fa590841131
Junien <manciet@hotmail.fr>
2024-02-27 14:57:12 +0100

    Ajout entité Client.java extends Societe

---
+++ src/entites/Client.java
@@ -0,0 +1,28 @@
+package entites;
+
+public class Client extends Societe {
+    private double chiffreAffaires;
+    private int nbrEmployes;
+
+    public Client(String raisonSociale, Adresse adresse, String telephone, String eMail, String commentaires, double chiffreAffaires, int nbrEmployes) {
+        super(raisonSociale, adresse, telephone, eMail, commentaires);
+        setChiffreAffaires(chiffreAffaires);
+        setNbrEmployes(nbrEmployes);
+    }
+
+    public double getChiffreAffaires() {
+        return chiffreAffaires;
+    }
+
+    public void setChiffreAffaires(double chiffreAffaires) {
+        this.chiffreAffaires = chiffreAffaires;
+    }
+
+    public int getNbrEmployes() {
+        return nbrEmployes;
+    }
+
+    public void setNbrEmployes(int nbrEmployes) {
+        this.nbrEmployes = nbrEmployes;
+    }
+}
