789c4620f0243cd0702507227ede66d2e9a482e6
Junien <manciet@hotmail.fr>
2024-02-28 08:57:08 +0100

    Creation Class CLientDao et ProspectDao qui implémente IDao

---
+++ src/dao/CLientDao.java
@@ -0,0 +1,32 @@
+package dao;
+
+import entites.Client;
+
+import java.util.List;
+
+public class CLientDao implements IDao<Client>{
+    @Override
+    public List<Client> findAll() {
+        return null;
+    }
+
+    @Override
+    public Client findByName(String nom) {
+        return null;
+    }
+
+    @Override
+    public void create(Client entity) {
+
+    }
+
+    @Override
+    public void update(Client entity) {
+
+    }
+
+    @Override
+    public void delete(Client entity) {
+
+    }
+}
---
+++ src/dao/ProspectDao.java
@@ -0,0 +1,32 @@
+package dao;
+
+import entites.Prospect;
+
+import java.util.List;
+
+public class ProspectDao implements IDao<Prospect>{
+    @Override
+    public List<Prospect> findAll() {
+        return null;
+    }
+
+    @Override
+    public Prospect findByName(String nom) {
+        return null;
+    }
+
+    @Override
+    public void create(Prospect entity) {
+
+    }
+
+    @Override
+    public void update(Prospect entity) {
+
+    }
+
+    @Override
+    public void delete(Prospect entity) {
+
+    }
+}
